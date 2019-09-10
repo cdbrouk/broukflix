b10a2eef3770f2399fd4d42e1a065b35dd63cb53
cdbrouk <cdbrouk@gmail.com>
2019-09-10 21:14:28 +0900

    Updates

---
+++ precache-manifest.974a34a9c0ad7e2f2dafe7c76ff7510a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52e8c59b97df92366ee4679c2f58e445",
+    "url": "/broukflix/index.html"
+  },
+  {
+    "revision": "660bd94118897ac3ea7c",
+    "url": "/broukflix/static/js/2.cfc39470.chunk.js"
+  },
+  {
+    "revision": "6c153d4436aef0282075",
+    "url": "/broukflix/static/js/main.c3d48c63.chunk.js"
+  },
+  {
+    "revision": "80a11d3ce953b0d2d6b0",
+    "url": "/broukflix/static/js/runtime~main.9ecf2ea6.js"
+  },
+  {
+    "revision": "151130ccad2b01c12a3924909bf22a52",
+    "url": "/broukflix/static/media/noPoster.151130cc.png"
+  }
+]);
